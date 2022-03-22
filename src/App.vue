5f9c9dce06a355da3be01683e25ee12abe8c6628
SalmonFishCooked <1564309204@qq.com>
2022-03-22 09:25:45 +0800

    CAMPHERS_AD

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import CamphersAD from './views/CamphersAD.vue'
+import './common/font/font.css'
+</script>
+
+<template>
+  <CamphersAD />
+</template>
+
+<style>
+#app {
+  font-family: 'MiSans';
+}
+
+/* scrollbar style */
+::-webkit-scrollbar {
+  background-color: transparent;
+  width: 6px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #cfd6df;
+  border-radius: 10px;
+  -webkit-box-shadow: none;
+}
+
+::-webkit-scrollbar-track {
+  background-color: transparent;
+  border-radius: 10px;
+  -webkit-box-shadow: none;
+}
+
+:hover::-webkit-scrollbar-thumb {
+  background-color: #c5cbd4;
+}
+</style>
---
+++ src/views/CamphersAD.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="p-4 bg-main text-white">
+    <p class="flex items-center justify-center text-xl">CAMPHERS IN HERE</p>
+    <div class="py-24 flex">
+      <div class="mx-auto md:flex items-center">
+        <div class="md:mr-32 md:mb-0 mb-4 animate__animated animate__fadeInTopLeft">
+          <h1 class="text-5xl font-semibold">CAMPHERS</h1>
+          <div class="flex py-2">
+            <h1 class="text-4xl font-semibold py-2">校园表白墙</h1>
+            <img src="../assets/svg/love_svg.svg" class="w-14 h-14 ml-2 animate__animated animate__bounce animate__delay-1s">
+          </div>
+          <button class="text-black border-4 border-black p-4 rounded-xl hover:bg-gray-500 hover:text-white transition">IS DEVELOPING...</button>
+        </div>
+        <img src="../assets/svg/in_love.svg" class="w-60 h-60 animate__animated animate__zoomInDown animate__delay-1s">
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-gray-50">
+    <div class="flex justify-center pt-12 animate__animated animate__fadeInLeft">
+      <div class="p-4 rounded-2xl shadow-xl bg-white hover:shadow-2xl hover:scale-105 transition flex flex-col md:flex-row justify-center items-center mx-8 md:p-20">
+        <img src="../assets/svg/rainbow.svg" style="width: 260px;height: 260px;">
+        <div class="flex flex-col justify-center xl:ml-40 w-72">
+          <h2 class="text-4xl mb-4">CAMPHERS 的来源</h2>
+          <p class="text-gray-500">CAMPHERS 是由 Campus 和 Whisper 组合而来
+            的一个拼接词。顾名思义，CAMPHERS结合了校园
+            与表白墙，将这两个元素组合在一起，并且能让你在网络上悄悄地（匿名）告诉TA哦。</p>
+          <p class="text-gray-500 mt-4 font-semibold">名称贡献： TianJun</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center pt-12 animate__animated animate__fadeInLeft">
+      <div class="p-4 rounded-2xl shadow-xl bg-black text-white hover:shadow-2xl hover:scale-105 transition flex flex-col md:flex-row justify-center items-center mx-8 md:p-20">
+        <img src="../assets/svg/box.svg" style="width: 260px;height: 260px;">
+        <div class="flex flex-col justify-center xl:ml-40 w-72">
+          <h2 class="text-4xl mb-4">匿名发布</h2>
+          <p>CAMPHERS 可以匿名发布任何你想说的话，如果宝贝有
+            啥想对TA说的话，CAMPHERS 保证一定能帮宝贝保密哦！</p>
+          <p class="mt-4 font-semibold">想法贡献：SalmonFishy</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center pt-12 animate__animated animate__fadeInLeft">
+      <div class="p-4 rounded-2xl shadow-xl bg-white hover:shadow-2xl hover:scale-105 transition flex flex-col md:flex-row justify-center items-center mx-8 md:p-20">
+        <img src="../assets/svg/letter.svg" style="width: 260px;height: 260px;">
+        <div class="flex flex-col justify-center xl:ml-40 w-72">
+          <h2 class="text-4xl mb-4">CAMPHERS 的优点</h2>
+          <p class="text-gray-500">CAMPHERS 是一个可无限结合学校的 APP，是面向所有
+            学校的墙。以往我们会使用QQ或者微信来查看学校的表白
+            墙，但是这样有一个弊端，就是我们不能很方便的找到学校
+            的表白墙，也不能找到具体的表白帖，但是CAMPHERS完美
+            地解决了这些问题。</p>
+          <p class="text-gray-500 mt-4 font-semibold">来自 · CAMPHERS的优点</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center pt-12 animate__animated animate__fadeInLeft">
+      <div class="p-4 rounded-2xl shadow-xl bg-black text-white hover:shadow-2xl hover:scale-105 transition flex flex-col md:flex-row justify-center items-center mx-8 md:p-20">
+        <img src="../assets/svg/dialog.svg" style="width: 260px;height: 260px;">
+        <div class="flex flex-col justify-center xl:ml-40 w-72">
+          <h2 class="text-4xl mb-4">CAMPHERS·表白墙</h2>
+          <p>说出你想说，却一直不敢对TA说的话。
+            或许，TA也等了你很久......</p>
+          <p class="mt-4 font-semibold">想法贡献：EVERYONE</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center pt-12 animate__animated animate__fadeInLeft">
+      <div class="p-4 rounded-2xl shadow-xl bg-white hover:shadow-2xl hover:scale-105 transition flex flex-col md:flex-row justify-center items-center mx-8 md:p-20">
+        <img src="../assets/svg/gift.svg" style="width: 260px;height: 260px;">
+        <div class="flex flex-col justify-center xl:ml-40 w-72">
+          <h2 class="text-4xl mb-4">更多功能......</h2>
+          <p class="text-gray-500">CAMPHERS 是一个可无限发展的APP，结合了校园与互联网。
+            更多功能还请各位宝贝们向我们提出宝贵的建议呐。</p>
+          <p class="text-gray-500 mt-4 font-semibold">来自 · CAMPHERS的发展</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="border-b border-gray-400 my-10 mx-20"></div>
+    <div class="text-gray-600 flex flex-col justify-center items-center mb-8">
+      <p>CAMPHERS 欢迎每一位宝贝的加入。</p>
+      <p>敬请期待 CAMPHERS 的到来！</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import 'animate.css'
+</script>
+
+<style>
+.bg-main {
+  background-color: rgb(225, 129, 129);
+}
+</style>
